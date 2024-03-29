daee89c5ce4ec619aaff5024221161559849674e
phongngrbl <phongngo26082000@gmail.com>
2024-03-29 10:14:50 +0700

    Add metadata doi

--- arxiv/bs4/arxiv.py
+++ arxiv/bs4/arxiv.py
@@ -2,7 +2,6 @@ import os
 import json
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 
 src = '/home/rb025/PycharmProjects/Data/crawler_data/arxiv'
 
@@ -31,7 +30,6 @@ def crawl(url):
                         parse_page(page_url, catagory)
 def parse_page(page_url, catagory):
     os.makedirs(os.path.join(src, catagory), exist_ok=True)
-    dst_folder = os.path.join(src, catagory)
 
     response = requests.get(page_url)
     meta = {}
@@ -39,18 +37,19 @@ def parse_page(page_url, catagory):
         soup = BeautifulSoup(response.content, 'html.parser')
         for paper, subject in zip (soup.find_all("dt"), soup.find_all("dd")):
             if paper.find('a', title="Download PDF") is not None:
+                paper_soup = BeautifulSoup(requests.get(page_url[:17] + paper.find('a', title="Abstract")['href']).content, 'html.parser')
+                if response.status_code == 200:
+                    doi = paper_soup.find_all('td')[9].find('a')['href']
                 pdf_url = page_url[:17] + paper.find('a', title="Download PDF")['href']
-                id  = paper.find('a', title="Download PDF")['href'].split('/')[2]
+                id = paper.find('a', title="Download PDF")['href'].split('/')[2]
                 meta['Url'] = pdf_url
                 meta['ID'] = id
+                meta['Doi'] = doi
                 meta['Title'] = subject.find('div', class_='list-title mathjax').get_text(strip=True)
                 meta['Author(s)'] = subject.find('div', class_='list-authors').get_text(strip=True)
                 meta['Subjects'] = subject.find('div', class_='list-subjects').get_text(strip=True)
                 meta['Topic'] = catagory
                 meta['Year'] = '2024'
-                with open(f"{dst_folder}/{meta['ID']}.json",
-                          'w', encoding='utf-8') as f:
-                    json.dump(meta, f, ensure_ascii=False)
 
 if __name__ == '__main__':
     url = 'https://arxiv.org'
